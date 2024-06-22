34f2a69f0b0f674f2eae04c446749c75507e9a81
daniyalaamir110 <daniyal.amir110@gmail.com>
2024-06-22 17:08:47 +0500

    refactor: google oauth login exception handling and refactoring

--- syncytium/authentication/utils.py
+++ syncytium/authentication/utils.py
@@ -1,13 +1,72 @@
-from typing import Any, Dict
-
 import requests
 from django.conf import settings
+from django.contrib.auth import get_user_model
 from django.core.exceptions import ValidationError
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 
-GOOGLE_ID_TOKEN_INFO_URL = "https://www.googleapis.com/oauth2/v3/tokeninfo"
-GOOGLE_ACCESS_TOKEN_OBTAIN_URL = "https://oauth2.googleapis.com/token"
-GOOGLE_USER_INFO_URL = "https://www.googleapis.com/oauth2/v3/userinfo"
+User = get_user_model()
+
+
+class GoogleOAuthUtils:
+    """
+    Utility class for Google OAuth2
+    """
+
+    class Constants:
+        GOOGLE_ID_TOKEN_INFO_URL = "https://www.googleapis.com/oauth2/v3/tokeninfo"
+        GOOGLE_ACCESS_TOKEN_OBTAIN_URL = "https://oauth2.googleapis.com/token"
+        GOOGLE_USER_INFO_URL = "https://www.googleapis.com/oauth2/v3/userinfo"
+
+    @classmethod
+    def get_access_token(cls, code, redirect_uri):
+        """
+        Get access token from Google using the given code
+        """
+        data = {
+            "code": code,
+            "client_id": settings.GOOGLE_OAUTH2_CLIENT_ID,
+            "client_secret": settings.GOOGLE_OAUTH2_CLIENT_SECRET,
+            "redirect_uri": redirect_uri,
+            "grant_type": "authorization_code",
+        }
+
+        response = requests.post(
+            cls.Constants.GOOGLE_ACCESS_TOKEN_OBTAIN_URL, data=data
+        )
+
+        if not response.ok:
+            raise ValidationError("Failed to obtain access token from Google.")
+
+        access_token = response.json()["access_token"]
+
+        return access_token
+
+    @classmethod
+    def get_user_info(cls, access_token):
+        """
+        Get user info from Google using the given access token
+        """
+        response = requests.get(
+            cls.Constants.GOOGLE_USER_INFO_URL, params={"access_token": access_token}
+        )
+
+        if not response.ok:
+            raise ValidationError("Failed to obtain user info from Google.")
+
+        return response.json()
+
+
+def generate_username_from_email(email):
+    """
+    Generate a username from the given email
+    """
+    prefix = email.split("@")[0]
+    username = prefix
+    suffix = 0
+    while User.objects.filter(username=username).exists():
+        username = f"{prefix}{suffix}"
+        suffix += 1
+    return username
 
 
 def get_error_message(exc):
@@ -26,39 +85,3 @@ def generate_tokens_for_user(user):
     access_token = token_data.access_token
     refresh_token = token_data
     return access_token, refresh_token
-
-
-def google_get_access_token(code, redirect_uri):
-    """
-    Get access token from Google using the given code
-    """
-    data = {
-        "code": code,
-        "client_id": settings.GOOGLE_OAUTH2_CLIENT_ID,
-        "client_secret": settings.GOOGLE_OAUTH2_CLIENT_SECRET,
-        "redirect_uri": redirect_uri,
-        "grant_type": "authorization_code",
-    }
-
-    response = requests.post(GOOGLE_ACCESS_TOKEN_OBTAIN_URL, data=data)
-
-    print(response.json())
-
-    if not response.ok:
-        raise ValidationError("Failed to obtain access token from Google.")
-
-    access_token = response.json()["access_token"]
-
-    return access_token
-
-
-def google_get_user_info(access_token):
-    """
-    Get user info from Google using the given access token
-    """
-    response = requests.get(GOOGLE_USER_INFO_URL, params={"access_token": access_token})
-
-    if not response.ok:
-        raise ValidationError("Failed to obtain user info from Google.")
-
-    return response.json()
--- syncytium/authentication/views.py
+++ syncytium/authentication/views.py
@@ -2,22 +2,24 @@ from urllib.parse import urlencode
 
 from account.models import RegistrationMethod
 from account.serializers import UserSerializer
+from account.tasks import send_registration_email
 from django.conf import settings
 from django.contrib.auth import get_user_model
 from django.shortcuts import redirect
+from drf_spectacular.utils import OpenApiParameter, extend_schema
 from rest_framework.decorators import action
 from rest_framework.generics import RetrieveAPIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
-from rest_framework.status import HTTP_200_OK
+from rest_framework.status import HTTP_200_OK, HTTP_400_BAD_REQUEST
 from rest_framework.views import APIView
 
 from .mixins import ApiErrorsMixin, PublicApiMixin
 from .serializers import GoogleLoginInputSerializer
 from .utils import (
+    GoogleOAuthUtils,
     generate_tokens_for_user,
-    google_get_access_token,
-    google_get_user_info,
+    generate_username_from_email,
 )
 
 User = get_user_model()
@@ -42,8 +44,14 @@ class GoogleLoginAPIView(APIView, ApiErrorsMixin, PublicApiMixin):
     API view to login with Google.
     """
 
+    @extend_schema(
+        parameters=[
+            OpenApiParameter(name="code", type=str),
+            OpenApiParameter(name="error", type=str),
+        ],
+    )
     def get(self, request):
-        serializer = GoogleLoginInputSerializer(data=request.query_params)
+        serializer = GoogleLoginInputSerializer(data=request.GET)
         serializer.is_valid(raise_exception=True)
         validated_data = serializer.validated_data
 
@@ -57,36 +65,49 @@ class GoogleLoginAPIView(APIView, ApiErrorsMixin, PublicApiMixin):
             return redirect(f"{login_url}?{params}")
 
         redirect_uri = f"{settings.BASE_FRONTEND_URL}/google"
-        access_token = google_get_access_token(code, redirect_uri)
-        user_data = google_get_user_info(access_token)
 
         try:
-            user = User.objects.get(email=user_data["email"])
-            access_token, refresh_token = generate_tokens_for_user(user)
-            response_data = {
-                "user": UserSerializer(user).data,
-                "access_token": str(access_token),
-                "refresh_token": str(refresh_token),
-            }
-            return Response(response_data)
+            access_token = GoogleOAuthUtils.get_access_token(code, redirect_uri)
+        except:
+            return Response(
+                {"detail": "Could not get access token from Google."},
+                HTTP_400_BAD_REQUEST,
+            )
 
-        except User.DoesNotExist:
-            username = user_data["email"].split("@")[0]
+        try:
+            user_data = GoogleOAuthUtils.get_user_info(access_token)
+        except:
+            return Response(
+                {"detail": "Could not get user info from Google."},
+                HTTP_400_BAD_REQUEST,
+            )
+
+        email = user_data["email"]
+        user = User.objects.filter(email=email).first()
+
+        if not user:
+            username = generate_username_from_email(email)
             first_name = user_data.get("given_name", "")
             last_name = user_data.get("family_name", "")
 
             user = User.objects.create(
                 username=username,
-                email=user_data["email"],
+                email=email,
                 first_name=first_name,
                 last_name=last_name,
                 registration_method=RegistrationMethod.GOOGLE,
             )
+            send_registration_email.delay(user.id)
 
-            access_token, refresh_token = generate_tokens_for_user(user)
-            response_data = {
-                "user": UserSerializer(user).data,
-                "access_token": str(access_token),
-                "refresh_token": str(refresh_token),
-            }
-            return Response(response_data)
+        elif user.registration_method != RegistrationMethod.GOOGLE:
+            return Response(
+                {"detail": "Please login with password."},
+                HTTP_400_BAD_REQUEST,
+            )
+
+        access_token, refresh_token = generate_tokens_for_user(user)
+        response_data = {
+            "access_token": str(access_token),
+            "refresh_token": str(refresh_token),
+        }
+        return Response(response_data)
